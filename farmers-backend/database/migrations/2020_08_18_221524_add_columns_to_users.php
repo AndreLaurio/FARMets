edc34543188d347aafadc7d32154845ae63ce47a
Joshua Mamangun <mamangunjoshua@gmail.com>
2020-08-19 10:16:33 +0800

    Database Migration
    - Deleted pending_seller related tables
    - Added Valid ID Tyes Table

--- farmers-backend/database/migrations/2020_08_18_221524_add_columns_to_users.php
+++ farmers-backend/database/migrations/2020_08_18_221524_add_columns_to_users.php
@@ -25,14 +25,18 @@ class AddColumnsToUsers extends Migration
             $table->integer('num_following')->default(0);
 
             $table->string('seller_name', 100)->nullable();
-            $table->string('req_valid_id')->nullable();
+            $table->bigInteger('valid_id_type_id')->unsigned()->nullable();
+            $table->string('valid_id_path')->nullable();
             $table->string('location')->nullable();
             $table->double('rate')->default(0);
+            $table->boolean('is_approved')->default(false);
             $table->bigInteger('approved_by')->nullable();
             $table->dateTime('approved_date')->nullable();
 
             $table->foreign('user_type_id')
                   ->references('user_type_id')->on('user_types');
+            $table->foreign('valid_id_type_id')
+                  ->references('valid_id_type_id')->on('valid_id_types');
         });
     }
 
